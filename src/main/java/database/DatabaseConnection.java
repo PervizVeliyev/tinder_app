6dcfd1f068922dcb048d2991dfc1cbccccac325a
PervizVeliyev <veliyevperviz1@gmail.com>
2022-10-24 18:40:25 +0400

    create

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String url = System.getenv("Database_url");
-    private static final String username = System.getenv("Database_name");
-    private static final String password = System.getenv("Database_password");
+    private static final String url = "jdbc:postgresql://ec2-54-77-40-202.eu-west-1.compute.amazonaws.com:5432/dd69aa0lk9lb14";
+    private static final String username = "eajcvkgnnhmxcd";
+    private static final String password = "9e63bc9d92d640f736e44a93fa43659942c0940efb519cb916898c2060a658ee";
 
     private static Connection connection;
 
